07f5fa633ffe10816701c4427272c927e55e24c0
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-06-19 10:38:00 +0300

    Initial Commit

---
+++ PaymentScreenUI/PaymentScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentScreenUIApp.swift
+//  PaymentScreenUI
+//
+//  Created by Atakan Kömürcü on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaymentScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
