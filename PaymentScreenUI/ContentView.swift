7219151e1b2a4a6f5a938932ab013d9302a0e91e
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-06-19 12:49:13 +0300

    Top part

--- PaymentScreenUI/ContentView.swift
+++ PaymentScreenUI/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var width = UIScreen.main.bounds.width
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .top) {
+            VStack {
+                Rectangle()
+                    .frame(width: width, height: 300)
+                    .foregroundColor(.blue)
+                    .cornerRadius(30)
+                    .shadow(color: .blue, radius: 50, y:  7)
+                
+                Spacer(minLength: 0)
+            }
+            
+            VStack {
+                Text("Payment Method")
+                    .fontWeight(.bold)
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .padding(.top)
+                    .padding(.bottom)
+                
+                HStack {
+                    Circle()
+                        .stroke(Color.white, lineWidth: 3)
+                        .frame(width: 20, height: 20)
+                        .overlay {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 10, height: 10)
+                        }
+                    
+                    
+                    Rectangle()
+                        .frame(width: width * 0.25, height: 1)
+                        .foregroundColor(.white)
+                    
+                    Circle()
+                        .stroke(Color.white, lineWidth: 3)
+                        .frame(width: 20, height: 20)
+                    
+                    Rectangle()
+                        .frame(width: width * 0.25, height: 1)
+                        .foregroundColor(.white)
+                    
+                    Circle()
+                        .stroke(Color.white, lineWidth: 3)
+                        .frame(width: 20, height: 20)
+                }
+                
+                HStack(alignment: .center, spacing: width * 0.18) {
+                    Text("Payment")
+                        .foregroundColor(.white)
+                    
+                    Text("Shipping")
+                        .foregroundColor(.white)
+                    
+                    Text("Review")
+                        .foregroundColor(.white)
+                }
+            }
         }
-        .padding()
     }
 }
 
